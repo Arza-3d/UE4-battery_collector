766777f75ef96570f5eb899e6b185aaeda97acf4
Forest Guhareza <arza.3d@gmail.com>
2018-07-18 17:27:35 +0700

    the end of video 3

---
+++ Source/BatteryCollector/Pickup.h
@@ -0,0 +1,35 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Pickup.generated.h"
+
+UCLASS()
+class BATTERYCOLLECTOR_API APickup : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	APickup();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// in case we wanna do something with the mesh when the character pick it up, we need function that return the mesh
+	FORCEINLINE class UStaticMeshComponent* GetMesh() const { return PickupMesh; }
+
+private: // only can be accessed through this pickup code
+
+	// mesh that represent our pickup in the level
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Pickup", meta = (AllowPrivateAccess = "true"))
+	class UStaticMeshComponent* PickupMesh;
+	
+};
